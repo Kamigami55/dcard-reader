74dffbf2f3794ddcd348f264b54aa9d4290c7096
Kamigami55 <eason@easonchang.com>
2021-02-28 23:21:37 +0800

    Complete infinite scroll feature and prevent duplicate API call
    ref: https://dev.to/hunterjsbit/react-infinite-scroll-in-few-lines-588f

--- src/App.js
+++ src/App.js
@@ -4,35 +4,70 @@ import { apiGetPosts } from './apis/postsAPI'
 
 function App() {
   const [posts, setPosts] = React.useState([])
+  const [page, setPage] = React.useState(1)
+  const [isFetching, setIsFetching] = React.useState(false)
 
   const fetchMorePosts = async () => {
-    try {
-      const { data = [] } = await apiGetPosts()
+    if (isFetching) return
 
-      setPosts(
-        data.map((post) => ({
-          id: post?.id,
-          title: post?.title,
-          excerpt: post?.excerpt
+    setIsFetching(true)
+    try {
+      const idOfLastPost = posts.length > 0 ? posts[posts.length - 1].id : null
+      const { data = [] } = await apiGetPosts(idOfLastPost)
+
+      setPosts([
+        ...posts,
+        ...data.map((post) => ({
+          id: post.id,
+          title: post.title,
+          excerpt: post.excerpt
         }))
-      )
+      ])
     } catch (err) {
-      console.error(err)
+      window.console.error(err)
     }
+    setIsFetching(false)
   }
 
   React.useEffect(() => {
     fetchMorePosts()
+  }, [page])
+
+  const loader = React.useRef(null)
+
+  const handleObserver = (entities) => {
+    const target = entities[0]
+    if (target.isIntersecting) {
+      setPage((prevPage) => prevPage + 1)
+    }
+  }
+
+  React.useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: '20px',
+      threshold: 1.0
+    }
+    // initialize IntersectionObserver
+    // and attaching to Load More div
+    const observer = new window.IntersectionObserver(handleObserver, options)
+    if (loader.current) {
+      observer.observe(loader.current)
+    }
   }, [])
 
   return (
     <main>
       {posts.map((post) => (
-        <article>
+        <article key={post.id}>
           <h2>{post.title}</h2>
           <p>{post.excerpt}</p>
         </article>
       ))}
+
+      <div className='loading' ref={loader}>
+        <h2>Loading...</h2>
+      </div>
     </main>
   )
 }
